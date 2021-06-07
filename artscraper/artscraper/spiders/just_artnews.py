0ebdb1cc44db9b5562d07207588a5acb65f2c6b8
erik.roger.fuller <erik.roger.fuller@gmail.com>
2021-06-07 11:26:13 -0400

    artnews & art in america spider ready to run, sitemap follow flag set to only 2020s

--- artscraper/artscraper/spiders/just_artnews.py
+++ artscraper/artscraper/spiders/just_artnews.py
@@ -7,15 +7,31 @@ from scrapy.spiders import SitemapSpider
 
 class JustArtnewsSpider(SitemapSpider):
     name = 'just_artnews'
-    #allowed_domains = ['https://www.artnews.com/sitemap_index.xml']
-    sitemap_urls = ['https://www.artnews.com/post-sitemap202106.xml']
+    #allowed_domains
+    sitemap_urls = ['https://www.artnews.com/sitemap_index.xml']
+    #= ['https://www.artnews.com/post-sitemap202106.xml']
+
+    sitemap_follow = ['/post-sitemap202']
+
+    #def sitemap_filter(self, entries):
+    #   for entry in entries:
+    #        test = entry['loc'].split("/")
+    #        test = test[3]
+    #        try:
+    #            if test[:12] == "post-sitemap":
+    #                print(entry)
+    #                yield entry
+    #        except IndexError:
+    #            pass
 
     def parse(self, response):
         item = Hyperallergic_Dir_Item
         l = ItemLoader(item=Hyperallergic_Dir_Item(), response=response)
 
         l.add_xpath("title" , '//meta[@property="og:title"]/@content')
-        l.add_xpath("para", '//p[@class="p1"]/descendant-or-self::text()|//p[@class="p2"]/descendant-or-self::text()')
+        l.add_xpath("para", '//p[@class="p1"]/descendant-or-self::text()|//p[@class="p2"]/descendant-or-self::text()'
+                            '|//span[@class="s1"]/descendant-or-self::text()|//span[@class="s2"]/descendant-or-self::text()'
+                            '|//div[contains(@class,"a-content")]/p/descendant-or-self::text()')
 
         captions = ("captions", '//div[@class="post-content-image // "]/figure/div/div/img/@alt')
         l.add_xpath("images", '//div[@class="post-content-image // "]/figure/div/div/img/@data-lazy-src')
@@ -27,6 +43,12 @@ class JustArtnewsSpider(SitemapSpider):
 
         url = response.url
         l.add_value('url', url)
-        l.add_value('source', 'ARTNews')
+        test = url.split('/')
+        if test[3] == 'art-in-america':
+            l.add_value('source', "Art in America")
+            #print(test[3], "===art-in-america!!!!")
+        else:
+            l.add_value('source', 'ARTNews')
+            #print(test[3])
 
         yield l.load_item()
